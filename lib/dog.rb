c28bef2f4c92b0726b23da08947f581f3311ea26
Steven Hunal <stevehunal@gmail.com>
2019-09-10 23:28:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,5 +37,6 @@ def save
   else 
     DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?);", self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
 end 
 end 
